1d97ef1400e17f6225b5d0303d0c2c81743b71fe
elgatodorado <migbernabeumor@gmail.com>
2020-02-18 16:00:31 +0100

    Front

---
+++ app/src/main/java/es/fempa/estebanescobar/twitterxd/UltimosTweets.java
@@ -0,0 +1,14 @@
+package es.fempa.estebanescobar.twitterxd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UltimosTweets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ultimos_tweets);
+    }
+}
